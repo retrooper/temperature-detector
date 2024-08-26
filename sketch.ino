a7e2103ca477c4f5782f01406c44db8c2fc3cbd1
retrooper <retrooperdev@gmail.com>
2024-08-26 23:39:55 +0200

    first commit

---
+++ sketch.ino
@@ -0,0 +1,29 @@
+// Now turn this trash into treasure!
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 10, 9, 8, 7);
+
+const float BETA = 3950;
+int temperature = 0;
+void setup() {
+  // put your setup code here, to run once:
+  Serial1.begin(115200);
+  //Serial1.println("Hello, Raspberry Pi Pico W!");
+  lcd.begin(16, 2);
+  lcd.println("Detecting temperature");
+  delay(1000);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  int analogValue = analogRead(A0);
+  float celsius = 1 / (log(1 / (1023. / analogValue - 1)) / BETA + 1.0 / 298.15) - 273.15;
+  //Serial1.println(celsius);
+  if (temperature != celsius) {
+    temperature = celsius;
+    lcd.clear();
+    lcd.setCursor(2, 0);
+    lcd.print(temperature);
+    lcd.print(" degrees");
+  }
+  delay(3000); // this speeds up the simulation
+}
